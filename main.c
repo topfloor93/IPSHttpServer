14f40306b8284a4ca1e7c1476a93168cce9c7ef5
Unknown <123leej@naver.com>
2018-09-03 17:21:41 +0900

    payloadtest

--- main.c
+++ main.c
@@ -10,17 +10,25 @@ void route()
 {
     ROUTE_START()
 
-    ROUTE_GET("/help")
+    ROUTE_GET("/ips/logs")
     {
         printf("HTTP/1.1 200 OK\r\n\r\n");
         printf("Hello! You are using %s", request_header("User-Agent"));
     }
 
-    ROUTE_POST("/")
+    ROUTE_POST("/ips")
     {
         printf("HTTP/1.1 200 OK\r\n\r\n");
         printf("Wow, seems that you POSTed %d bytes. \r\n", payload_size);
-        //printf("you sent %s\n", &payload);
+        printf("you sent %s\n", payload);
+        printf("Fetch the data using `payload` variable.");
+    }
+
+    ROUTE_POST("/ips/rules")
+    {
+        printf("HTTP/1.1 200 OK\r\n\r\n");
+        printf("Wow, seems that you POSTed %d bytes. \r\n", payload_size);
+        printf("you sent %s\n", payload);
         printf("Fetch the data using `payload` variable.");
     }
 
