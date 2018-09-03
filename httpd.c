4748c7fd8fc0b16aaa7bd4c301d2bd0d090976c6
Unknown <123leej@naver.com>
2018-09-03 17:51:54 +0900

    1

--- httpd.c
+++ httpd.c
@@ -169,7 +169,7 @@ void respond(int n)
         t2 = request_header("Content-Length"); // and the related header if there is  
         payload = t;
         payload_size = t2 ? atol(t2) : (rcvd-(t-buf));
-        while (payload < *payload + payload_size)
+        while (*payload < *payload + payload_size)
         {
             printf("%s\n",payload);
             payload++;
